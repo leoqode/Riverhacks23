b13eae032917dfea8cf2ae0d6c02f838676030af
theMehrad <mehrad.jahani@g.austincc.edu>
2023-05-18 15:58:20 -0500

    Landing page editied

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import logo from "../assets/mindfulU_logo.png"
+import './LandingPage.css';
 const LandingPage = () => {
+  
+
+  
   return (
-    <div>LandingPage</div>
+    <div>
+    <header>
+      <title>Your Page Title</title>
+
+      
+      <img src={logo} alt="a gorgeous logo" style={{ width: '200px', height: 'auto' }}/>
+    
+      
+    </header>
+   <body>
+   <div>Welcome! Ready to feel good?</div>
+    <button class="landingPageButton">Login</button>
+    <button class="landingPageButton">Sign Up</button>
+   </body>
+
+   <footer>
+
+    <div> Contact us!</div>
+   </footer>
+   
+   </div>
   )
+  
 }
 
 export default LandingPage
