24b64d575003facb48c416de7e70e4c206535d1d
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2023-05-19 00:24:44 -0500

    Task Item proper alignments
    consider using div to align text and checkbox together but this is OPTIONAL

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -5,17 +5,16 @@ import droplet from "../assets/droplet.png";
 const TaskItem = ({task, onToggle}) => {
     return (
         <div className="task-item">
-        <span><input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => onToggle(task.id)}
-        /></span>
-        <h3>{task.title}</h3>
-        <div className="task-weight">
-        {task.weight}<img className="point" src={droplet} alt="droplet"/>
-        </div>
+            <span><input type="checkbox" checked={task.completed} onChange={() => onToggle(task.id)}/></span>
+            <h3>{task.title}</h3>
+            <div className="task-rewards">
+                <div className="point">
+                    {task.weight}<img src={droplet} alt="droplet"/>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default TaskItem;
+
