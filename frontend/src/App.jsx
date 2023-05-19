3ae3f88207170ff79e834c64bd42a9db4634f91d
Leo <angel.jimenez2@g.austincc.edu>
2023-05-19 11:36:35 -0500

    Commit to fix conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,57 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import About from "./pages/About";
-import Game from "./pages/Game";
-import Profile from "./pages/Profile";
-import Social from "./pages/Social";
-import NavRoute from "./components/NavRoute";
-import ProtectedRoute from "./components/ProtectedRoute";
-
+import { BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import About from './pages/About'
+import Game from './pages/Game'
+import Profile from './pages/Profile';
+import Social from './pages/Social';
+import NavRoute from './components/NavRoute'
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
   return (
     <div className="App w-full h-full">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/about" element={<About />} />
 
-          <Route element={<NavRoute />}>
+
+        <Routes>
+          <Route
+                path='/about'
+                element={<About setUser={(user) => {setUser(user);navigate('/game')}}/>}
+                />
+
+          <Route element={<NavRoute/>} >
             {/* routes with nav here */}
 
-            <Route
-              element={<ProtectedRoute authed={true} redirectPath="/about" />}
-            >
-              <Route path="/game" element={<Game />} />
+            <Route element={<ProtectedRoute authed={!!user} redirectPath='/about'/>}>
 
-              <Route path="/user" element={<Profile />} />
+              <Route
+                path='/game'
+                element={<Game/>}
+                />
 
-              <Route path="/mindful" element={<Social />} />
+              <Route 
+                path='/user' 
+                element={<Profile/>}
+                />
+
+              <Route
+                path='/mindful'
+                element={<Social/>}
+                />
+
             </Route>
+
           </Route>
 
-          <Route path="*" element={<Navigate to="/about" replace />} />
+          <Route path='' element={<Navigate to='/about' replace/>}/>
+
         </Routes>
-      </BrowserRouter>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
