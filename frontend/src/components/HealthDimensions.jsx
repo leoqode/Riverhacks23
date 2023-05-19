6f4e41448f719e158202c5e5e468da9efd8466d5
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2023-05-18 21:42:44 -0500

    Health Dimensions button changes and sprites for "game"

--- frontend/src/components/HealthDimensions.jsx
+++ frontend/src/components/HealthDimensions.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./HealthDimensions.css";
 
 
 const HealthDimensions = () => {
   return (
     <div className="d-inline">
-      <button id="physical-btn" className="btn btn-primary mr-2">Physical</button>
-      <button id="emotional-btn" className="btn btn-primary mr-2">Emotional</button>
-      <button id="intellectual-btn" className="btn btn-primary mr-2">Intellectual</button>
-      <button id="spiritual-btn" className="btn btn-primary mr-2">Spiritual</button>
-      <button id="social-btn" className="btn btn-primary">Social</button>
+      <button id="physical-btn" className="btn physical-btn">Physical</button>
+      <button id="emotional-btn" className="btn emotional-btn">Emotional</button>
+      <button id="intellectual-btn" className="btn intellectual-btn">Intellectual</button>
+      <button id="spiritual-btn" className="btn spiritual-btn">Spiritual</button>
+      <button id="social-btn" className="btn social-btn">Social</button>
     </div>
   );
 };
--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -6,13 +6,12 @@ const TaskItem = ({task, onToggle}) => {
     return (
         <div className="task-item">
         <h3>{task.title}</h3>
-        <p>{task.weight}</p>
-        <img src={droplet} alt="droplet" />
         <input
             type="checkbox"
             checked={task.completed}
             onChange={() => onToggle(task.id)}
         />
+        <div>{task.weight}<img class="point" src={droplet} alt="droplet" /></div>
         </div>
     );
 }
