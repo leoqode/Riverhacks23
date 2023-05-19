7fad771273e69b4b046aa409d0a6c5e1e78edd99
powerreddude <daidanmoore@gmail.com>
2023-05-19 01:53:54 -0500

    change between task types

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,12 +1,19 @@
 import TaskItem from "../components/TaskItem.jsx";
 import React from "react";
+import { useState } from "react";
 import sampleTasks from "../json/sampleTasks.json";
 import HealthDimensions from "../components/HealthDimensions.jsx";
 import SaplingView from "../components/SaplingView.jsx";
 
 function Game() {
   
+  const [dimension, setDimension] = useState("Physical");
+
   const tasks = sampleTasks;
+
+  const handleChangeDimensions = (dimension) => {
+    setDimension(dimension);
+  };
   
   const handleToggle = (taskId) => {
     console.log("Task toggled:", taskId);
@@ -15,12 +22,10 @@ function Game() {
   return (
     <>
     <SaplingView/>
-    <HealthDimensions/>
-    {Object.values(tasks).flatMap((dimensionTasks) =>
-        dimensionTasks.map((task) => (
-          <TaskItem key={task.title} task={task} onToggle={handleToggle}/>
-        ))
-    )}
+    <HealthDimensions handleChange={handleChangeDimensions}/>
+    {sampleTasks[dimension].map((task) => (
+        <TaskItem key={task.title} task={task} onToggle={handleToggle}/>
+      ))}
 
 
     </>
