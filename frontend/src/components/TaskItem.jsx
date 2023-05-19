e1c88952a688111dcc8d797025b738143dedc2c7
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2023-05-19 00:01:04 -0500

    Game component fixes

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -5,13 +5,15 @@ import droplet from "../assets/droplet.png";
 const TaskItem = ({task, onToggle}) => {
     return (
         <div className="task-item">
-        <h3>{task.title}</h3>
-        <input
+        <span><input
             type="checkbox"
             checked={task.completed}
             onChange={() => onToggle(task.id)}
-        />
+        /></span>
+        <h3>{task.title}</h3>
+        <div className="task-weight">
         {task.weight}<img className="point" src={droplet} alt="droplet"/>
+        </div>
         </div>
     );
 }
